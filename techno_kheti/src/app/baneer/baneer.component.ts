28799e4e53500cb028ce79c486e36c413f1c2949
suryakumarseenivasankgit push origin suryakumargit config --global user.name suryakumarseenivasank <suryakumar.s.2022@gmail.com>
2022-09-16 15:38:05 +0530

    all files

---
+++ techno_kheti/src/app/baneer/baneer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-baneer',
+  templateUrl: './baneer.component.html',
+  styleUrls: ['./baneer.component.scss']
+})
+export class BaneerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
