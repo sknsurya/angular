59ecbcbcac35c0be7955b5f30dd9a90149b220a7
suryakumarseenivasankgit push origin suryakumargit config --global user.name suryakumarseenivasank <suryakumar.s.2022@gmail.com>
2022-09-19 13:18:36 +0530

    header component ts file

--- first_app/src/app/header/header.component.ts
+++ first_app/src/app/header/header.component.ts
@@ -6,39 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  a:any
-  isDisabled:boolean= false
-  twoWayBinding : any;
+  s:any
+  // isDisabled:boolean= false
+  // twoWayBinding : any;
   
-  isEnable : boolean = true
+  // isEnable : boolean = true
  
 
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-   ////two way controller<------------->view(task one)
-   data(event : any){
-    if(event.target.value){
-      this.isEnable = false
-      return;
-    }
-    this.isEnable = true
-  }
-  enable(){
-    this.twoWayBinding = ""
-    this.isEnable = true
-  }
+  ngOnInit(): void {}
 
-//disable button 
-  myButton(event:any){
-    console.log("hi",event);
-    this.isDisabled = true
-  }
-  keyPress(events:any){
-     console.log(events.target.value);
-     this.a=events.target.value
+  mybtn(event:any){
+    console.log("iam suryakumar",event);
 
 }
 }
