ad2aa96481199b5b32823f05f6b7bae95f6e1b7c
suryakumarseenivasankgit push origin suryakumargit config --global user.name suryakumarseenivasank <suryakumar.s.2022@gmail.com>
2022-09-20 17:44:09 +0530

    commit

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListUserComponent,
+    UserDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
