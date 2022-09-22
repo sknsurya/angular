1cf773ce7f07c0e2b1bc8cf6672dafd1b9ac2169
suryakumarseenivasankgit push origin suryakumargit config --global user.name suryakumarseenivasank <suryakumar.s.2022@gmail.com>
2022-09-22 21:55:46 +0530

    app routing module ts file

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AboutAsComponent } from './about-as/about-as.component';
 import { ContectComponent } from './contect/contect.component';
 import { Component1Component } from './component1/component1.component';
 import { Component2Component } from './component2/component2.component';
+import { ErrormsgComponent } from './errormsg/errormsg.component';
 
 
 
@@ -12,19 +13,23 @@ const routes: Routes = [
   {
     path: "homecomponent",
     component: HomeComponent
-   
   },
   {
     path: "aboutascomponent",
     component: AboutAsComponent, children: [{
       path: 'comp1',
       component: Component1Component
-    },{
-      path:'comp2',
-      component:Component2Component
+    }, {
+      path: 'comp2',
+      component: Component2Component
     }]
   },
-  { path: "contectcomponent", component: ContectComponent }];
+  { path: "contectcomponent", component: ContectComponent },
+  {
+    path: "**",
+    component: ErrormsgComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
