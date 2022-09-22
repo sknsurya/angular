7f842653ead961e736c7bdea1cca3ea1a58b0fb1
suryakumarseenivasankgit push origin suryakumargit config --global user.name suryakumarseenivasank <suryakumar.s.2022@gmail.com>
2022-09-22 21:56:59 +0530

    error msg component ts file

---
+++ routing/src/app/errormsg/errormsg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-errormsg',
+  templateUrl: './errormsg.component.html',
+  styleUrls: ['./errormsg.component.scss']
+})
+export class ErrormsgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
