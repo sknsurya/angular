28810e8225cd5c3868ac54bae9256a7140093056
suryakumarseenivasankgit push origin suryakumargit config --global user.name suryakumarseenivasank <suryakumar.s.2022@gmail.com>
2022-09-22 12:20:41 +0530

    chid component ts file

--- first_app/src/app/child/child.component.ts
+++ first_app/src/app/child/child.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Input, OnInit, Output  } from '@angular/core';
 
 export class ChildComponent implements OnInit {
   @Input() user : any;
-  newUser : any=' '
+  newUser : any=' ' 
   @Output() addNewUser = new EventEmitter(); 
   constructor() { }
 
