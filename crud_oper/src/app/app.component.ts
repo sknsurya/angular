738c3deeccae50fb35a4cd5e9ccace78ae9e9f21
suryakumarseenivasankgit push origin suryakumargit config --global user.name suryakumarseenivasank <suryakumar.s.2022@gmail.com>
2022-09-27 15:39:51 +0530

    all

--- crud_oper/src/app/app.component.ts
+++ crud_oper/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
+import { NgModule } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crud_oper';
+  userlist: any
+  name: any;
+  email: any;
+
+  constructor(private appService: AppService) { }
+
+  get() {
+    this.appService.getalluser().subscribe(data => {
+      this.userlist = data;
+    }
+    )
+  }
+  // insertuser() {
+  //   this.appService.insertalluser(name: this.name, this.email: this.email).subscribe((newuser) => {
+  //    console.log(newuser)
+  //    this.router.navigate(['../'])
+  //   }
+  //   )
+  // }
 }
--- crud_oper/src/app/app.module.ts
+++ crud_oper/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Router } from 'express';
+import { FormComponent } from './form/form.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { subscribeOn } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    UserListComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
