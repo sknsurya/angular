26c381a7a89d60699e5e0cccc4b4b2b6a914e820
suryakumarseenivasankgit push origin suryakumargit config --global user.name suryakumarseenivasank <suryakumar.s.2022@gmail.com>
2022-09-28 10:36:37 +0530

    form component ts file

--- crud/src/app/form/form.component.ts
+++ crud/src/app/form/form.component.ts
@@ -1,15 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { AppService } from '../app.service';
+import { NgModel } from '@angular/forms';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
+  name: any
+  email: any
+  constructor(private appservice: AppService) { }
 
   ngOnInit(): void {
+  }
+  adduser() {
+    console.log(this.name)
+    this.appservice.addnewuser({"name" : this.name, "email" : this.email}).subscribe((body)=>{
+      console.log(body)
+      
+
+    })
   }
 
 }
