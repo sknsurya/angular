23778f830dcbee2ce094a98c145c9f4ef0919901
suryakumarseenivasankgit push origin suryakumargit config --global user.name suryakumarseenivasank <suryakumar.s.2022@gmail.com>
2022-09-28 10:35:56 +0530

    app service ts ile

--- crud/src/app/app.service.ts
+++ crud/src/app/app.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
+ 
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+  getalluser(){
+    return this.httpClient.get(environment.appurl+"/getuser")
+  }
+
+  addnewuser(body:any){
+    return this.httpClient.post(environment.appurl+'/insertuser',body)
+  }
 }
